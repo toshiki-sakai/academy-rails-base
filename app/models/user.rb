3e4c8cfa6a5ab1fb9b86a65b002f674bb47646b0
酒井俊輝 <toshiki7124@gmail.com>
2024-04-15 00:25:02 +0900

    User新規登録時のエラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 255 }
+  validates :name, presence: { message: '氏名は必ず入力してください' },
+                   length: { maximum: 255, message: '氏名は255文字以内で入力してください' }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: true
+  validates :email, presence: { message: 'メールアドレスは必ず入力してください' },
+                    length: { maximum: 255, message: 'メールアドレスは255文字以内で入力してください' },
+                    format: { with: VALID_EMAIL_REGEX , message: 'メールアドレスが正しい形式ではありません'},
+                    uniqueness: { message: 'そのメールアドレスはすでに存在しています' }
   has_secure_password
   VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
-  validates :password, presence: true, length: { minimum: 8 }, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数を両方含む必要があります"}
+  validates :password, presence: { message: 'パスワードは必ず入力してください' },
+                       length: { minimum: 8, message: '英数字8文字以上で入力してください' },
+                       format: { with: VALID_PASSWORD_REGEX, message: '英数字8文字以上で入力してください'}
 end

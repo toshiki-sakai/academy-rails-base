78649b2bad7fb05ef40f2fa8b53d8c2f5127875b
酒井俊輝 <toshiki7124@gmail.com>
2024-04-20 02:03:09 +0900

    パスワードとメールアドレスが一致しないエラーメッセージ追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id]=user.id
       redirect_to root_path, notice: 'ログインしました'
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'メールアドレス、もしくはパスワードが間違っています'
       render :new, status: :unprocessable_entity
     end
   end

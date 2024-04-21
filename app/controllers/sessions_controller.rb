2b5aaf360d2314b6202ec0129be54c757b79f310
酒井俊輝 <toshiki7124@gmail.com>
2024-04-21 22:34:59 +0900

    ユーザーログイン機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,12 @@
 class SessionsController < ApplicationController
-  skip_before_action :login_required
-
   def new
   end
 
   def create
-    user=User.find_by(email: session_params[:session][:email].downcase)
-    if user&.authenticate(session_params[:session][:password])
+    @user = User.find_by(email: session_params[:session][:email].downcase)
+    if @user&.authenticate(session_params[:session][:password])
       reset_session
-      session[:user_id]=user.id
+      session[:user_id] = @user.id
       redirect_to root_path, notice: 'ログインしました'
     else
       flash.now[:danger] = 'メールアドレス、もしくはパスワードが間違っています'

ba428e1d38e161c4c1acddb6fa4670cf80bc7207
酒井俊輝 <toshiki7124@gmail.com>
2024-04-25 01:32:00 +0900

    ログイン時のエラーメッセージ追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
     if @user&.authenticate(params[:session][:password])
       reset_session
       session[:user_id] = @user.id
-      redirect_to root_path, notice: 'ログインしました'
+      redirect_to root_path
     else
-      flash.now[:danger] = 'メールアドレス、もしくはパスワードが間違っています'
+      flash[:danger] = 'メールアドレス、もしくはパスワードが間違っています'
       render :new, status: :unprocessable_entity
     end
   end

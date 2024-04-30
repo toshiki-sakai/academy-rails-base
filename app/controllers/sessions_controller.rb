125d5006c780d7ccfab45f8bb3c13d448d4c45b0
酒井俊輝 <toshiki7124@gmail.com>
2024-04-30 22:13:50 +0900

    ログイン失敗時にメールアドレスの入力を保持

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,31 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user= User.find_by(email: params[:session][:email].downcase)
-    if @user&.authenticate(params[:session][:password])
-      reset_session
-      session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      flash[:danger] = 'メールアドレス、もしくはパスワードが間違っています'
+    if params[:session][:email].blank? && params[:session][:password].blank?
+      flash[:danger] = 'メールアドレスとパスワードを入力してください'
       render :new, status: :unprocessable_entity
+    else
+      @user= User.find_by(email: params[:session][:email].downcase)
+      if @user&.authenticate(params[:session][:password])
+        reset_session
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        @user = User.new(session_params)
+        flash[:danger] = 'メールアドレス、もしくはパスワードが間違っています'
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
   def destroy
     session.delete(:user_id)
     redirect_to login_url, notice: "ログアウトしました。"
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end

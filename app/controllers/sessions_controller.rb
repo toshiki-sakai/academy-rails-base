42ae9fbdea0300897bf1930bdaa050f46a6d2edb
酒井俊輝 <toshiki7124@gmail.com>
2024-04-30 19:57:02 +0900

    エラーメッセージ修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,21 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     if params[:session][:email].blank? && params[:session][:password].blank?
+      flash[:danger] = 'メールアドレスとパスワードを入力してください'
       render :new, status: :unprocessable_entity
     else
       @user= User.find_by(email: params[:session][:email].downcase)
       if @user&.authenticate(params[:session][:password])
-      reset_session
-      session[:user_id] = @user.id
-      redirect_to root_path
+        reset_session
+        session[:user_id] = @user.id
+        redirect_to root_path
       else
-      flash[:danger] = 'メールアドレス、もしくはパスワードが間違っています'
-      render :new, status: :unprocessable_entity
+        flash[:danger] = 'メールアドレス、もしくはパスワードが間違っています'
+        render :new, status: :unprocessable_entity
       end
     end
   end

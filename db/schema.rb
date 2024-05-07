6e6de0889724bf70dc687aef42fa799d36cacd76
酒井俊輝 <toshiki7124@gmail.com>
2024-05-07 14:17:26 +0900

    マイグレーションファイルを作り直す

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_08_044934) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_07_041452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

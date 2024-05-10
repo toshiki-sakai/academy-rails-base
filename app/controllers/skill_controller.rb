ad7b6961cfe755190c7d19d660c6b3e265383025
酒井俊輝 <toshiki7124@gmail.com>
2024-05-11 02:52:59 +0900

    skillビューを簡単に作成

---
+++ app/controllers/skill_controller.rb
@@ -0,0 +1,16 @@
+class SkillController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :avatar, :email, :password, :password_confirmation, :content, :image)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get 'skill/:id/edit', to: 'skill#edit'
+  get 'skill/:id/new', to: 'skill#new'
   resources :users
   resources :profiles
   resources :sessions
+  resources :skill
 end

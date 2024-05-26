70781ad7de8dcd9446b258e8d7eaefe35857b956
酒井俊輝 <toshiki7124@gmail.com>
2024-05-27 03:06:49 +0900

    過去三ヶ月分プルダウン選択できるようにする

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -5,11 +5,10 @@ class SkillsController < ApplicationController
     @current_month = Date.today.month
     @categories = Category.all
     @learning_data = LearningDatum.where(user: @user, month: @current_month)
-    @skills = Skill.where(user: @user)
   end
 
   def create_learning_data
-    @learning_data = LearningData.new(learning_data_params)
+    @learning_data = LearningDatum.new(learning_data_params)
     @learning_data.user = current_user
 
     if @learning_data.save
@@ -22,6 +21,6 @@ class SkillsController < ApplicationController
   private
 
     def learning_data_params
-      params.require(:learning_data).permit(:skill, :time, :month)
+      params.require(:learning_datum).permit(:category_id, :skill, :time, :month)
     end
 end

2e75f374b4e76bec591f8702a15f901e014652f5
酒井俊輝 <toshiki7124@gmail.com>
2024-06-03 16:35:49 +0900

    userにskillをネストする

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,15 +1,16 @@
 class SkillsController < ApplicationController
+  before_action :set_user
+  before_action :set_learning_datum, only: [:edit, :update]
 
   def edit
-    @user = User.find(params[:id])
     @current_month = Date.today.month
     @categories = Category.all
-    @learning_data = LearningDatum.where(user: @user, month: @current_month)
+    @learning_data = LearningDatum.where(user: @user, month: @current_month).first
   end
 
-  def create_learning_data
+  def create
     @learning_data = LearningDatum.new(learning_data_params)
-    @learning_data.user = current_user
+    @learning_data.user = @user
 
     if @learning_data.save
       render json: { success: true, message: '学習時間が保存されました。' }
@@ -18,9 +19,25 @@ class SkillsController < ApplicationController
     end
   end
 
+  def update
+    if @learning_data.update(learning_data_params)
+      redirect_to edit_user_skill_path(@user, @learning_data), notice: '学習時間が更新されました。'
+    else
+      render :edit
+    end
+  end
+
   private
 
-    def learning_data_params
-      params.require(:learning_datum).permit(:category_id, :skill, :time, :month)
-    end
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_learning_datum
+    @learning_data = LearningDatum.find(params[:id])
+  end
+
+  def learning_data_params
+    params.require(:learning_datum).permit(:category_id, :skill, :time, :month)
+  end
 end
